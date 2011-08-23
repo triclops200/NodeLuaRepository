4c280696f2a71171edfe0ddd5b9b50d8de76f2c0
Bryan Hoyle <starfoxprime@gmail.com>
2011-08-23 17:47:24 -0400

    Basic server for sending a file from a given ID

---
+++ NodeServer.js
@@ -0,0 +1,13 @@
+var net = require('net');
+var s = net.Server(function(socket){
+var authdata;
+socket.on('data',function(data){
+var fs = require('fs');
+var st = "" +data;
+data = st.substring(0,st.length-1);
+fs.readFile('./'+data+'.lua', function (err, data) {
+  socket.write(data+"\n"+String.fromCharCode(4));
+});
+});
+});
+s.listen(10457);
