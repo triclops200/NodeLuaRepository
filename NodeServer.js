daeb7e71d28fc0346f12921bddf03f3783595734
Bryan Hoyle <starfoxprime@gmail.com>
2011-08-23 20:59:25 -0400

    Security update

--- NodeServer.js
+++ NodeServer.js
@@ -5,9 +5,20 @@ socket.on('data',function(data){
 var fs = require('fs');
 var st = "" +data;
 data = st.substring(0,st.length-1);
+var good = true;
+for(i = 0; i < data.length; i++)
+{
+if(data[i].charCodeAt(0)<48 ||data[i].charCodeAt(0)>57&& good == true)
+   {
+    console.log("Cracking attempt from: "+socket.remoteAddress+"\n");
+    good = false;
+    }
+}
+if(good == true){
 fs.readFile('./'+data+'.lua', function (err, data) {
   socket.write(data+"\n",'ascii');
 });
+}
 });
 });
 s.listen(10457);
